69520f225cec28163f6adda122a24dce174b7b3e
wantao <13813872403>
2022-07-15 17:15:44 +0800

    'merge'

--- src/libs/dialog/dialog.tsx
+++ src/libs/dialog/dialog.tsx
@@ -3,13 +3,19 @@ import d from './dialog.module.scss'
 export const Dialog = defineComponent({
     props: { title: { type: String, default: '' }, content: { type: String, default: '' } },
     setup(props, context) {
+        const onCancel = () => {
+
+        }
+        const onConfirm = () => {
+
+        }
         return () => (<div class={d.dialogWrap}>
             <div class={d.content}>
                 <header>{props.title}</header>
                 <main>{props.content}</main>
                 <footer>
-                    <span>取消</span>
-                    <span>确定</span>
+                    <span onClick={onCancel}>取消</span>
+                    <span onClick={onConfirm}>确定</span>
                 </footer>
             </div>
         </div>)
